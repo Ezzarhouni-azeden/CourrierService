fe1c30ce5037e52d2fd7eab55ec10146584ea8a2
Azd <azedenezzair@gmail.com>
2025-02-26 16:02:33 +0000

    Initial commit

---
+++ src/main/java/com/example/courrierservice/CourrierServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.courrierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourrierServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourrierServiceApplication.class, args);
+    }
+
+}
